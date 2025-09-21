bd0c395347c63948969b93b52b21de5cfa049480
Sofiia3339 <sofia.soboleva@knu.ua>
2025-09-21 23:17:14 +0300

    Update Functional_Object.cpp

--- Functional_Object.cpp
+++ Functional_Object.cpp
@@ -13,3 +13,21 @@ Functional_Object::~Functional_Object() {}
 pair<double,double> Functional_Object::getX(){
     return x0;
 }
+
+pair<double,double> Functional_Object::operator()()
+{
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distrib(0, arrb.size()-1);
+    int rnd = distrib(gen);
+    pair<double,double> y;
+    if (count == 0) {
+        y = x0;
+    }
+    else {
+        y = make_pair((x0.first+arrb[rnd].first)/2, (x0.second + arrb[rnd].second) / 2);
+        x0 = y;
+    }
+    count++;
+    return y;
+}
