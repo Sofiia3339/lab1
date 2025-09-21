8d10d42f9d903d601e16290c5ae7f7359f83e9d5
Sofiia3339 <sofia.soboleva@knu.ua>
2025-09-21 19:07:55 +0300

    Update Functional_Object.cpp

--- Functional_Object.cpp
+++ Functional_Object.cpp
@@ -1,5 +1,15 @@
-//
-// Created by Sofiia Soboleva on 21.09.2025.
-//
-
 #include "Functional_Object.h"
+
+Functional_Object::Functional_Object(pair<double, double>& x0, vector<pair<double,double>>& arrb) {
+    this->x0 = x0;
+    this->arrb = arrb;
+}
+
+Functional_Object::~Functional_Object()
+{
+}
+
+pair<double,double> Functional_Object::getX()
+{
+    return x0;
+}
