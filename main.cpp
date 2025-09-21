df7bacc5bf323807257212150c80f918ce83c449
Sofiia3339 <sofia.soboleva@knu.ua>
2025-09-21 23:26:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,3 +16,25 @@ void readFromFile(int& n, pair<double,double>& x, vector<pair<double,double>>& n
     file.close();
 }
 
+int main() {
+    int n;
+    pair<double, double>x;
+    vector<pair<double,double>> numbers;
+    readFromFile(n, x, numbers);
+    Functional_Object obj = Functional_Object(x, numbers);
+    ofstream file("output.txt");
+    if (!file) {
+        cout << "Unable to open file output.txt" << endl;
+    }
+    else {
+        file << n << endl;
+        file << obj.getX().first << " " << obj.getX().second << endl;
+        pair<double, double> y;
+        for (int i = 0; i < n; i++) {
+            y = obj();
+            file << y.first << " " << y.second << endl;
+        }
+        file.close();
+        cout << "Closed sucsesfully" << endl;
+    }
+}
