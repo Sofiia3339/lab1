afa4f9802c08d6cc1c63222bf9f7ac8b66d4b5a6
Sofiia3339 <sofia.soboleva@knu.ua>
2025-09-21 19:17:36 +0300

    Update Functional_Object.cpp

--- Functional_Object.cpp
+++ Functional_Object.cpp
@@ -3,13 +3,13 @@
 Functional_Object::Functional_Object(pair<double, double>& x0, vector<pair<double,double>>& arrb) {
     this->x0 = x0;
     this->arrb = arrb;
+    if (arrb.empty()) {
+        throw invalid_argument("Input array cannot be empty.");
+    }
 }
 
-Functional_Object::~Functional_Object()
-{
-}
+Functional_Object::~Functional_Object() {}
 
-pair<double,double> Functional_Object::getX()
-{
+pair<double,double> Functional_Object::getX(){
     return x0;
 }
