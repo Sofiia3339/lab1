a6a8d59cac430d4f56e12d0ea7d4131fc634d27c
Sofiia3339 <sofia.soboleva@knu.ua>
2025-09-21 23:21:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
-//
-// Created by Sofiia Soboleva on 21.09.2025.
-//
+#include"Functional_Object.h"
+#include<fstream>
+using namespace std;
+
+void readFromFile(int& n, pair<double,double>& x, vector<pair<double,double>>& numbers) {
+    ifstream file("input.txt");
+    if (!file) {
+        cout << "Unable to open file input.txt" << endl;
+        return;
+    }
+    file >> n >> x.first >> x.second;
+    pair<int,int> num;
+    while (file >> num.first >> num.second) {
+        numbers.push_back(num);
+    }
+    file.close();
+}
+
