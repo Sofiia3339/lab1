88cffbe08bd2b64ababe1884d2090c5df9713115
Sofiia3339 <sofia.soboleva@knu.ua>
2025-09-22 00:31:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
-#include"Functional_Object.h"
-#include<fstream>
+// Clang (C++20)
+
+#include "Functional_Object.h"
+#include <fstream>
 using namespace std;
 
-void readFromFile(int& n, pair<double,double>& x, vector<pair<double,double>>& numbers) {
+void readFromFile(int& n, pair<double, double>& x, vector<pair<double, double>>& numbers) {
     ifstream file("input.txt");
     if (!file) {
         cout << "Unable to open file input.txt" << endl;
         return;
     }
     file >> n >> x.first >> x.second;
-    pair<int,int> num;
+    pair<int, int> num;
     while (file >> num.first >> num.second) {
         numbers.push_back(num);
     }
@@ -18,23 +20,21 @@ void readFromFile(int& n, pair<double,double>& x, vector<pair<double,double>>& n
 
 int main() {
     int n;
-    pair<double, double>x;
-    vector<pair<double,double>> numbers;
+    pair<double, double> x;
+    vector<pair<double, double>> numbers;
     readFromFile(n, x, numbers);
     Functional_Object obj = Functional_Object(x, numbers);
     ofstream file("output.txt");
     if (!file) {
         cout << "Unable to open file output.txt" << endl;
-    }
-    else {
-        file << n << endl;
+    } else {
         file << obj.getX().first << " " << obj.getX().second << endl;
         pair<double, double> y;
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             y = obj();
             file << y.first << " " << y.second << endl;
         }
         file.close();
-        cout << "Closed sucsesfully" << endl;
+        cout << "Closed successfully" << endl;
     }
 }
